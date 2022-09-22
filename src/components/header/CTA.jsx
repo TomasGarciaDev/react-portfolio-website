130c234bbf51f50c853772b5783b6f2cee081b63
Tomas Garcia <tomasgarciadev@gmail.com>
2022-09-22 10:26:31 +0200

    ME image added to Header

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,8 +3,8 @@ import CV from '../../assets/Tomas Garcia_English_CV.pdf'
 export default function CTA() {
   return (
     <div className="cta">
-      <a href={CV} download>Download CV</a>
-      <a href="#contact">Contact</a>
+      <a href={CV} download className='btn'>Download CV</a>
+      <a href="#contact" className='btn btn-primary'>Contact</a>
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import './header.css';
-import CTA from './CTA'
+import CTA from './CTA';
+import ME from '../../assets/ME_header.png'
 
 export default function Header() {
   return (
@@ -9,6 +10,10 @@ export default function Header() {
         <h1>Tomás García</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA />
+
+        <div className='me'>
+          <img src={ME} />
+        </div>
       </div>
     </header>
   )
