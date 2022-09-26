061e38120ce61210971a61198025fa3408be0c23
Tomas Garcia <tomasgarciadev@gmail.com>
2022-09-26 12:02:37 +0200

    Importing aabout image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,20 @@
 import "./about.css";
+import ME from '../../assets/ME_about.png';
 
 export default function About() {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h2>About me</h2>
+
+      <div className='container about__container'>
+        <div className={ME}>
+          <img src={ME} alt=''/>
+        </div>
+      </div>
+
+        <div className='about_content'>
+
+        </div>
+    </section>
   )
 }
