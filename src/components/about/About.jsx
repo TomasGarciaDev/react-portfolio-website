f314a2a8e2382c16b407c87c28bb9a772fb4c535
Tomas Garcia <tomasgarciadev@gmail.com>
2022-09-27 15:55:56 +0200

    JSX for About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,6 +15,14 @@ export default function About() {
         <div className='about_content'>
 
         </div>
+
+        <p>
+        I am an enthusiastic and proactive Full-Stack developer and Musician. I love teamwork synergies, and I work hard to get a good job done while building a fun and healthy working environment. I use my experience as a musician to feed my work as a Developer, skills I consistently apply in teamwork settings.
+I trained as a coder at Makers Academy, Europe’s #1 Developer Bootcamp. I started as an eager amateur coder; this led me to win a Full Scholarship at Makers Academy. These intensive 16 weeks of Full-Stack training helped me become a competent and confident Developer.
+I highly enjoy and love the continuous growth aspect of a tech career. My main goal is to grow my knowledge as a Full-Stack Developer because I also love working with React and helping make my clients and companies thrive.
+        </p>
+
+        <a href="#contact" className="btn btn-primary">Contact</a>
     </section>
   )
 }
