2a3987aa38e3818950944345005886fd5d429ab3
Tomas Garcia <tomasgarciadev@gmail.com>
2022-09-29 12:53:45 +0200

    Basic technologies Experience info for Frontend (Experience component)

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,7 +1,66 @@
 import './experience.css';
+import {AiFillCheckCircle} from 'react-icons/ai'
 
 export default function Experience() {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>The Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>Tailwind</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>React</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>Next</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+        <div className='experience__backend'>
+        <h3>Backend Development</h3>
+          <div className='experience__content'>
+            
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
