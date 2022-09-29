68e2872df27bf6cb0487ce44f82156b4494545a5
Tomas Garcia <tomasgarciadev@gmail.com>
2022-09-29 13:00:10 +0200

    Mock information for Backend for the Experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -55,9 +55,39 @@ export default function Experience() {
           </div>
         </div>
         <div className='experience__backend'>
+        {/* END OF FRONT END */}
+
         <h3>Backend Development</h3>
           <div className='experience__content'>
-            
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>Node JS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>MongoDB</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>PHP</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>MySQL</h4>
+              <small className='text-light'>Basic</small>
+            </article>
+
+            <article className='experience__details'>
+              <AiFillCheckCircle />
+              <h4>Python</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
           </div>
         </div>
       </div>
